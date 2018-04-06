2fa9e39c6b826490f1f5acc99c82efb03e003550
ruanshiron <sandt26@gmail.com>
2018-04-06 10:54:19 +0700

    Chef-Nodejs Update Login/Register

---
+++ chef-nodejs/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Theten12'
+    },
+	'database': 'my_schema',
+    'users_table': 'users'
+};
